6ab9d5c4e5d126dc57ca6af69579d20a8dbe01c1
Ubuntu <ubuntu@ip-172-31-17-19.ec2.internal>
2022-01-01 06:38:41 +0000

    delete file

--- testscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! docker info > /dev/null 2>&1; then
-  echo "This script uses docker, and it isn't running - please start docker and try again!"
-  exit 1
-else
-	echo "docker running in container"
-fi
