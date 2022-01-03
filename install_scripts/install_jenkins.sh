d10d0fd4b81627219a700562666aec62a6d5fcd4
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-01-03 21:54:36 +0000

    update jenkins installer

--- install_scripts/install_jenkins.sh
+++ install_scripts/install_jenkins.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker run --rm -d -u root --name jenkins-container -p 8080:8080 -p 50000:50000 --network minikube -v ~/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean 
+sudo docker run --rm -d -u root --name jenkins-container -p 8080:8080 -p 50000:50000 --network minikube -v ~/jenkins_data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home jenkinsci/blueocean 
